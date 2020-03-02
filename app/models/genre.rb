765ef42a5a9feb906af07cd2c128e98b384650a1
Teerth Patel <teerth1205@gmail.com>
2020-03-02 02:48:52 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+   self.artists.count  # return the number of artists associated with the genre
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.artists # return an array of strings containing every musician's name
   end
 end
